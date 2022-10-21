e3cc4e7d2a67e5cd572ddb53810478caa2770933
JesusM3dina <contrerasjesusmedina@gmail.com>
2022-10-20 20:50:05 -0500

    primera version progessbar

--- src/assets/components/TotalProyecto.vue
+++ src/assets/components/TotalProyecto.vue
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-6 my-2">
             <h3>
-
+                
                 Total Proyectos: {{ numeroProyectos }}
             </h3>
         </div>
@@ -12,7 +12,7 @@
     </div>
 
     <div class="table-responsive">
-
+       
         <table class="table table-dark table-hover">
             <thead>
                 <tr>
@@ -21,6 +21,8 @@
                     <th>Tipo</th>
                     <th>Urgente</th>
                     <th>Completado</th>
+                    <th>borrar</th>
+                    
                 </tr>
             </thead>
 
@@ -29,9 +31,13 @@
                     <td>{{ index+1 }}</td>
                     <td> {{ proyecto.proyecto }} </td>
                     <td> {{ proyecto.tipo }} </td>
-
+                   
+                    
                     <td @click="cambiarEstado(proyecto, 'urgente')" :class="proyecto.urgente ? 'bg-success' : 'bg-danger'"> {{ proyecto.urgente ? "Si" : "No" }} </td>
                     <td @click="cambiarEstado(proyecto, 'completado')" :class="proyecto.completado ? 'bg-success' : 'bg-danger'"> {{ proyecto.completado ? "Completo" : "Incompleto" }} </td>
+                    <td @click="borrar(index)">
+                        borrar
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -41,6 +47,6 @@
 
 <script>
     export default{
-        props: ['numeroProyectos','proyectos', 'cambiarEstado', 'limpiarData'],
+        props: ['numeroProyectos','proyectos', 'cambiarEstado', 'limpiarData','borrar'],
     };
 </script>
